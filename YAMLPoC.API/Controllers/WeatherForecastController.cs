87f3c5d4b9cfcf9dcf09c9c044981cc2505d1a2e
Joe Ipe <joe.ipe@hotmail.com>
2022-05-31 17:07:40 +1000

    init

---
+++ YAMLPoC.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace YAMLPoC.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConfiguration _config;
+
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> GetWeatherForecast()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        public ActionResult GetConfig()
+        {
+            var list = new List<string>
+            {
+                _config["ApiSettings"],
+                "vault"
+            };
+
+            return Ok(list);
+        }
+    }
+}
